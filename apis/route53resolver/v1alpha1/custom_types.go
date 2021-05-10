ee94280a9d04c0245de3b7bf6a40a0d5ea617935
Amna Irfan <amirfan@ea.com>
2021-05-10 11:49:31 -0700

    add route53 resolver endpoint and resolver rule resource
    Signed-off-by: Amna Irfan <amirfan@ea.com>
    
    added route53resolverrule
    
    Signed-off-by: Amna Irfan <amirfan@ea.com>
    
    remove resolverquerylogconfig resource
    
    Signed-off-by: Amna Irfan <amirfan@ea.com>
    
    fixed formatting after running make reviewable test
    
    Signed-off-by: Amna Irfan <amirfan@ea.com>
    
    remove binaries

---
+++ apis/route53resolver/v1alpha1/custom_types.go
@@ -0,0 +1,56 @@
+package v1alpha1
+
+import xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+
+// CustomResolverEndpointParameters are custom parameters for ResolverEndpoint
+type CustomResolverEndpointParameters struct {
+	// The ID of one or more security groups that you want to use to control access
+	// to this VPC. The security group that you specify must include one or more
+	// inbound rules (for inbound Resolver endpoints) or outbound rules (for outbound
+	// Resolver endpoints). Inbound and outbound rules must allow TCP and UDP access.
+	// For inbound access, open port 53. For outbound access, open the port that
+	// you're using for DNS queries on your network.
+	SecurityGroupIDs []string `json:"securityGroupIDs,omitempty"`
+	// SecurityGroupIDRefs is a list of references to SecurityGroups used to set
+	// the SecurityGroupIDs.
+	// +optional
+	SecurityGroupIDRefs []xpv1.Reference `json:"securityGroupIdRefs,omitempty"`
+
+	// SecurityGroupIDsSelector selects references to SecurityGroupID used
+	// to set the SecurityGroupIDs.
+	// +optional
+	SecurityGroupIDSelector *xpv1.Selector `json:"securityGroupIdSelector,omitempty"`
+
+	IPAddresses []*IPAddressRequest `json:"ipAddresses"`
+}
+
+// CustomResolverRuleParameters are custom parameters for CustomResolverRule
+type CustomResolverRuleParameters struct {
+	// ResolverEndpointIDRef is the reference to the ResolverEndpoint used
+	// to set the ResolverEndpointID
+	// +optional
+	ResolverEndpointIDRef *xpv1.Reference `json:"resolverEndpointIdRefs,omitempty"`
+	// ResolverEndpointIDSelector selects references to ResolverEndpoint used
+	// to set the ResolverEndpointID
+	// +optional
+	ResolverEndpointIDSelector *xpv1.Selector `json:"resolverEndpointIdSelector,omitempty"`
+}
+
+// CustomResolverQueryLogConfigParameters are custom parameters for CustomResolverQueryLogConfig
+type CustomResolverQueryLogConfigParameters struct {
+}
+
+// IPAddressRequest is used by ResolverEndpoint
+type IPAddressRequest struct {
+	IP *string `json:"ip,omitempty"`
+
+	SubnetID *string `json:"subnetID,omitempty"`
+
+	// SubnetIDRefs is a list of references to Subnets used to set
+	// the SubnetIDs.
+	SubnetIDRef *xpv1.Reference `json:"subnetIdRef,omitempty"`
+
+	// SubnetIDSelector selects references to Subnets used
+	// to set the SubnetIDs.
+	SubnetIDSelector *xpv1.Selector `json:"subnetIdSelector,omitempty"`
+}
